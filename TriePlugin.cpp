a7767fb726402faf51b10e3a2186467c5e49dca2
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2021-07-27 17:48:45 -0400

    removed python2 path

--- TriePlugin.cpp
+++ TriePlugin.cpp
@@ -13,9 +13,11 @@ void TriePlugin::run() {
 
 void TriePlugin::output(std::string file) {
 //beta_diversity.py -i filtered_otu_table.biom -m euclidean,weighted_unifrac,unweighted_unifrac -t rep_set.tre -o beta_div
-   std::string command = "export OLDPATH=${PYTHONPATH}; export PYTHONPATH=${PYTHON2_DIST_PACKAGES}:${PYTHON2_SITE_PACKAGES}:${PYTHONPATH}; pick_otus.py ";
+   //std::string command = "export OLDPATH=${PYTHONPATH}; export PYTHONPATH=${PYTHON2_DIST_PACKAGES}:${PYTHON2_SITE_PACKAGES}:${PYTHONPATH}; pick_otus.py ";
+   std::string command = "pick_otus.py ";
  command += "-i "+inputfile+" -m trie ";
- command += "-o "+file+"; cp "+file+"/*.txt "+file+"/..; export PYTHONPATH=OLDPATH";
+ //command += "-o "+file+"; cp "+file+"/*.txt "+file+"/..; export PYTHONPATH=OLDPATH";
+ command += "-o "+file+"; cp "+file+"/*.txt "+file+"/..";
  std::cout << command << std::endl;
 
  system(command.c_str());
